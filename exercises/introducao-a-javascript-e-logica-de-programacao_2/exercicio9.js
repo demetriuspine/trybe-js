1cfe04a0ac1b0fd7415459d3fe8287494f626c5a
lucas <mr.pine97@gmail.com>
2021-06-24 20:17:46 -0300

    resolução do exercício 9

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio9.js
@@ -0,0 +1,10 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+let novaArray = [];
+for (index = 1; index <= 25; index += 1) {
+  novaArray.push(index)
+}
+arrayDiv = [] 
+for (indexDiv = 0; indexDiv < novaArray.length; indexDiv += 1) {
+  arrayDiv.push(novaArray[indexDiv]/2)
+}
+console.log(arrayDiv)
